b359fab98676151470ae8c23972448470de5eb4e
Rambuk01 <festersenmario@gmail.com>
2023-02-16 16:56:00 +0100

    added a lot of funktionality

--- application.py
+++ application.py
@@ -1,42 +1,20 @@
-from flask import Flask, render_template, url_for
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-
-
+from flask import Flask, render_template, url_for, request
+from request_data import * # this script handles getting the data use get_jobs_municipality(search_key)
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
 app = application
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    data = ""
+    data_paragraph = "";
+    if request.method == 'POST':
+        """Here I will handle the data from the form"""
+        data = get_jobs_municipality("IT underviser")
+        form = request.form
+    return render_template('index.html', divdata = data)
 
 
-
-"""
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>KanbanBean</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-"""
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
--- readme.txt
+++ readme.txt
@@ -2,3 +2,7 @@ kanbanbean
 A kanban board on AWS elastic beanstalk
 
 Code pipeline from git to awssta
+
+
+dont forget:
+https://curlconverter.com/python/
